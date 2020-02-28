3a8cc2e105feba7c1c8b8fc1f9e86faf5a72bb24
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-02-28 01:05:36 -0300

    [Add] iniciando testes: CardHead

---
+++ __tests__/components/CardHead.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {render} from '@testing-library/react-native';
+
+import CardHead from '~/components/CardHead';
+
+describe('CardHead', () => {
+  it('Should be able to show user image', () => {});
+
+  it('Should be able to show user name', () => {});
+});
