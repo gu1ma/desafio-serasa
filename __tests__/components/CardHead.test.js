0e923b5ce40c76d197741e2778bee16fa5c1a0cd
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-02-29 14:20:47 -0300

    [Add] Primeiros testes no componente CardHead

--- __tests__/components/CardHead.test.js
+++ __tests__/components/CardHead.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
 import {render} from '@testing-library/react-native';
 
 import CardHead from '~/components/CardHead';
 
 describe('CardHead', () => {
-  it('Should be able to show user image', () => {});
+  const userData = {
+    userName: 'Rick Sanches',
+    userPhoto: {
+      uri:
+        'https://i.pinimg.com/originals/e6/22/d2/e622d2fdccb6fdf31133dd7ab273892a.jpg',
+    },
+    score: 25,
+    scoreDescription: 'Sua pontuação é baixa',
+  };
 
-  it('Should be able to show user name', () => {});
+  it('Should have the userimage Image component', () => {
+    const {getByTestId} = render(<CardHead userData={userData} />);
+
+    expect(getByTestId('userphoto')).toBeTruthy();
+  });
+
+  it('Should have the usename Text component', () => {
+    const {getByTestId} = render(<CardHead userData={userData} />);
+
+    expect(getByTestId('username')).toBeTruthy();
+  });
 });
--- src/components/CardHead/index.js
+++ src/components/CardHead/index.js
@@ -16,9 +16,9 @@ export default function CardHead({
         shadowColor={borderColor}
         radius={70}
         color="#fff">
-        <UserPhoto source={userData.userPhoto} />
+        <UserPhoto testID="userphoto" source={userData.userPhoto} />
       </ProgressCircle>
-      <UserName>{userData.userName}</UserName>
+      <UserName testID="username">{userData.userName}</UserName>
     </Container>
   );
 }
