842ebc9a7ad4e296cd2efcf774ea43ab70595fa9
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-06-13 20:24:41 -0300

    test: teste no reducer de credito

--- __tests__/components/store/reducer/main.test.js
+++ __tests__/components/store/reducer/main.test.js
@@ -1,5 +1,6 @@
 import * as Score from '~/store/modules/main/actions';
 import reducer, {INITIAL_STATE} from '~/store/modules/main/reducer';
+import {MOCK_DEBIT_STATE, MOCK_CREDIT_STATE} from '../../../data/main';
 
 describe('Should test user data reducer', () => {
   it('@main/GET_USER_DATA_REQUEST', () => {
@@ -68,25 +69,11 @@ describe('Should test user data reducer', () => {
 });
 
 describe('Should test debt reducer', () => {
-  const MOCK_STATE = {
-    userData: {
-      score: 25,
-      scoreDescription: 'Mock pontuation description',
-      scoreStatus: 'low',
-      scoreLevel: 0,
-      debtData: [],
-      protectionPlainData: null,
-      creditData: null,
-    },
-    loading: false,
-    error: false,
-  };
-
   it('@main/PAY_DEBT_REQUEST', () => {
-    const state = reducer(MOCK_STATE, Score.payDebtRequest(20, 1));
+    const state = reducer(MOCK_DEBIT_STATE, Score.payDebtRequest(20, 1));
 
     const stateResponse = {
-      ...MOCK_STATE,
+      ...MOCK_DEBIT_STATE,
       loading: true,
     };
 
@@ -105,7 +92,7 @@ describe('Should test debt reducer', () => {
     };
 
     const state = reducer(
-      MOCK_STATE,
+      MOCK_DEBIT_STATE,
       Score.payDebtSuccess(
         mockResponse.score,
         2,
@@ -124,10 +111,82 @@ describe('Should test debt reducer', () => {
   });
 
   it('@main/PAY_DEBT_ERROR', () => {
-    const state = reducer(MOCK_STATE, Score.payDebtError());
+    const state = reducer(MOCK_DEBIT_STATE, Score.payDebtError());
 
     expect(state).toStrictEqual({
-      userData: MOCK_STATE.userData,
+      userData: MOCK_DEBIT_STATE.userData,
+      loading: false,
+      error: true,
+    });
+  });
+});
+
+describe('Should test credit reducer', () => {
+  it('@main/ACCEPT_CREDIT_REQUEST', () => {
+    const state = reducer(MOCK_CREDIT_STATE, Score.acceptCreditRequest(20, 1));
+    const stateResponse = {
+      ...MOCK_CREDIT_STATE,
+      loading: true,
+    };
+
+    expect(state).toStrictEqual(stateResponse);
+  });
+
+  it('@main/ACCEPT_CREDIT_SUCCESS', () => {
+    const mockResponse = {
+      score: 45,
+      scoreDescription: 'Mock pontuation description',
+      scoreStatus: 'low',
+      scoreLevel: 0,
+      debtData: [],
+      protectionPlainData: [[]],
+      creditData: [],
+    };
+
+    const state = reducer(
+      MOCK_CREDIT_STATE,
+      Score.acceptCreditSuccess(
+        mockResponse.score,
+        2,
+        mockResponse.scoreDescription,
+        mockResponse.scoreLevel,
+        mockResponse.creditData,
+        mockResponse.protectionPlainData
+      )
+    );
+
+    expect(state).toStrictEqual({
+      userData: mockResponse,
+      loading: false,
+      error: false,
+    });
+  });
+
+  it('@main/ACCEPT_CREDIT_ERROR', () => {
+    const mockResponse = {
+      score: 25,
+      scoreDescription: 'Mock pontuation description',
+      scoreStatus: 'low',
+      scoreLevel: 0,
+      debtData: [],
+      protectionPlainData: null,
+      creditData: [],
+    };
+
+    const state = reducer(
+      MOCK_CREDIT_STATE,
+      Score.acceptCreditError(
+        mockResponse.score,
+        2,
+        mockResponse.scoreDescription,
+        mockResponse.scoreLevel,
+        mockResponse.creditData,
+        mockResponse.protectionPlainData
+      )
+    );
+
+    expect(state).toStrictEqual({
+      userData: mockResponse,
       loading: false,
       error: true,
     });
---
+++ __tests__/data/main.js
@@ -0,0 +1,27 @@
+export const MOCK_DEBIT_STATE = {
+  userData: {
+    score: 25,
+    scoreDescription: 'Mock pontuation description',
+    scoreStatus: 'low',
+    scoreLevel: 0,
+    debtData: [],
+    protectionPlainData: null,
+    creditData: null,
+  },
+  loading: false,
+  error: false,
+};
+
+export const MOCK_CREDIT_STATE = {
+  userData: {
+    score: 25,
+    scoreDescription: 'Mock pontuation description',
+    scoreStatus: 'low',
+    scoreLevel: 0,
+    debtData: [],
+    protectionPlainData: null,
+    creditData: [],
+  },
+  loading: false,
+  error: false,
+};
